89a68b1b7f629a3505614a7f6660e8a29cc0e247
Your Name <you@example.com>
2014-03-06 23:13:22 +0000

    initial controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 YelpClone::Application.routes.draw do
+  root 'places#index'
+  resources :places
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
