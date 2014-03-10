79f004298fc960a15b5abf8adce74b2739c8fd47
Ken Mazaika <kmazaika@gmail.com>
2014-03-10 16:50:51 -0400

    Hook up the mailer

--- app/models/place.rb
+++ app/models/place.rb
@@ -5,4 +5,12 @@ class Place < ActiveRecord::Base
   belongs_to :user
   geocoded_by :address
   after_validation :geocode
+  after_create :deliver_admin_email
+
+
+  private
+
+  def deliver_admin_email
+    NotificationMailer.place_added(self).deliver
+  end
 end
--- test/models/place_test.rb
+++ test/models/place_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class PlaceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "email is sent when a place is save" do
+    admin = FactoryGirl.create(:user, :admin => true)
+    place = FactoryGirl.build(:place)
+    ActionMailer::Base.deliveries.clear
+    assert ActionMailer::Base.deliveries.empty?
+
+    assert_difference 'ActionMailer::Base.deliveries.count' do
+      place.save
+    end
+
+    email = ActionMailer::Base.deliveries.last
+    assert_equal 'A new place has been added to the application', email.subject
+  end
 end
