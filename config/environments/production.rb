a48151087e69f29b4dddc0e8db06a6ca929aeda9
marcomorawec <marco.morawec@gmail.com>
2014-03-21 16:19:58 -0400

    added the sendgrid addon so emailing and comment functions work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,17 @@ YelpClone::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'yelp-clone.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'nomster.herokuapp.com' }
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+  
 
 end
